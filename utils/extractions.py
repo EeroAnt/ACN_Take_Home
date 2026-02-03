f067af04cc70ad2399c69b647416a2cabb9b07b3
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-03 20:25:33 +0200

    exclude the headers from the customer extraction

--- utils/extractions.py
+++ utils/extractions.py
@@ -3,7 +3,7 @@ def extract_from_csv(filepath: str) -> list[tuple]:
   results = []
 
   with open(filepath, "r") as file:
-    for line in file.readlines():
+    for line in file.readlines()[1:]:
       results.append(tuple(line.strip().split(",")))
 
   return results
