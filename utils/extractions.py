ea5359f2b50c2d67b81c6336d19fba29505d1c08
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-03 20:35:06 +0200

    add comment

--- utils/extractions.py
+++ utils/extractions.py
@@ -3,6 +3,7 @@ def extract_from_csv(filepath: str) -> list[tuple]:
   results = []
 
   with open(filepath, "r") as file:
+    # both csv's in the task had headers, thus hardcoded [1:]
     for line in file.readlines()[1:]:
       results.append(tuple(line.strip().split(",")))
 
