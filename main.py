bb5a5a02d18d1d118b1fd3965cb7dcaf0a29acf9
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-03 20:46:32 +0200

    transform transactions

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from utils.database import populate_customers_table, setup_database
 from utils.extractions import extract_from_csv
-from utils.transformations import transform_customers
+from utils.transformations import transform_customers, transform_transactions
 
 def main():
     conn = setup_database()
     customers = extract_from_csv("data/customers.csv")
     customers_transformed = transform_customers(customers)
     populate_customers_table(conn, customers_transformed)
-    # transactions = extract_from_csv("data/transactions.csv")
+    transactions = extract_from_csv("data/transactions.csv")
+    transactions_transformed = transform_transactions(transactions)
 
 if __name__ == "__main__":
     main()
--- utils/transformations.py
+++ utils/transformations.py
@@ -1,5 +1,19 @@
 def transform_customers(raw_data: list[tuple]) -> list[tuple]:
-    return [
-        (int(customer_id), country, signup_date, email)
-        for customer_id, country, signup_date, email in raw_data
-    ]
+  return [
+    (int(customer_id), country, signup_date, email)
+    for customer_id, country, signup_date, email in raw_data
+  ]
+
+def transform_transactions(raw_data: list[tuple]) -> list[tuple]:
+  # There were rows without a customer id, but at this point I'll just extract everything
+  return [
+    (
+      int(transaction_id),
+      int(customer_id) if customer_id else None,
+      float(amount),
+      currency.upper(),
+      timestamp,
+      category if category else None
+    )
+    for transaction_id, customer_id, amount, currency, timestamp, category in raw_data
+  ]
