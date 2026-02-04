8dbdd7eafc0427ccaa80e225db122b7454a92462
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-04 18:02:32 +0200

    added basic progress logging

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from utils.database import populate_customers_table, populate_transactions_table, setup_database
 from utils.extractions import extract_from_csv
+from utils.logging_config import logger
 from utils.transformations import transform_customers, transform_transactions
 
 def main():
+    logger.info("Starting the pipeline")
     conn = setup_database()
-    
+    logger.info("Database setup done.")
+
     customers = extract_from_csv("data/customers.csv")
+    logger.info(f"Extracted {len(customers)} customers")
     customers_transformed = transform_customers(customers)
+    logger.info("Customer data transformed")
     populate_customers_table(conn, customers_transformed)
-    
+    logger.info("Customer data loaded to database")
+
     transactions = extract_from_csv("data/transactions.csv")
+    logger.info(f"Extracted {len(customers)} transactions")
     transactions_transformed = transform_transactions(transactions)
+    logger.info("Transaction data transformed")
     populate_transactions_table(conn, transactions_transformed)
+    logger.info("Transaction data loaded to database")
 
 if __name__ == "__main__":
     main()
