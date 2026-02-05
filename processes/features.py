2297d91c4b5d93055d4243ce2a6cda59fc63f1a9
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-05 19:26:42 +0200

    Add churning risk feature

--- processes/features.py
+++ processes/features.py
@@ -2,8 +2,9 @@ import pandas as pd
 import sqlite3
 
 def feature_report(conn: sqlite3.Connection) -> pd.DataFrame:
-  df = get_feature_dataframe(conn)
-  preview_features(df)
+  feature_df = get_feature_dataframe(conn)
+  churning_risk_df = get_churning_risk_df(conn)
+  preview_features(feature_df, churning_risk_df)
     
 
 def get_feature_dataframe(conn: sqlite3.Connection) -> pd.DataFrame:
@@ -21,20 +22,38 @@ def get_feature_dataframe(conn: sqlite3.Connection) -> pd.DataFrame:
           ROUND(
               (JULIANDAY(MAX(timestamp)) - JULIANDAY(MIN(timestamp))) / NULLIF(COUNT(*) - 1, 0), 
               1
-          ) as avg_days_between_transactions,
-          ROUND(JULIANDAY('2020-12-31') - JULIANDAY(MAX(timestamp))) as days_since_last_transaction
+          ) as avg_days_between_transactions
       FROM clean_transactions
       GROUP BY customer_id
     """,
     conn
   )
 
-def preview_features(df: pd.DataFrame) -> None:
-  show_top_ten(df, "avg_days_between_transactions", True)
-  show_top_ten(df, "total_spent", False)
-  show_top_ten(df, "transaction_count", False)
-  show_top_ten(df, "max_amount", False)
-  show_top_ten(df, "days_since_last_transaction", False)
+def get_churning_risk_df(conn: sqlite3.Connection) -> pd.DataFrame:
+  return pd.read_sql_query(
+    """
+      SELECT 
+          customer_id,
+          COUNT(*) as transaction_count,
+          SUM(amount) as total_spent,
+          ROUND(
+              (JULIANDAY(MAX(timestamp)) - JULIANDAY(MIN(timestamp))) / NULLIF(COUNT(*) - 1, 0), 
+              1
+          ) as avg_days_between_transactions,
+          ROUND(JULIANDAY('2020-12-31') - JULIANDAY(MAX(timestamp))) as days_since_last_transaction
+      FROM clean_transactions
+      GROUP BY customer_id
+      HAVING transaction_count > 4
+    """,
+    conn
+  )
+
+def preview_features(feature_df: pd.DataFrame, churning_risk_df: pd.DataFrame) -> None:
+  show_top_ten(feature_df, "avg_days_between_transactions", True)
+  show_top_ten(feature_df, "total_spent", False)
+  show_top_ten(feature_df, "transaction_count", False)
+  show_top_ten(feature_df, "max_amount", False)
+  show_top_ten(churning_risk_df, "days_since_last_transaction", False)
 
 def show_top_ten(df: pd.DataFrame, column: str, ascending: bool) -> None:
   sorted_df = df.sort_values(column, ascending=ascending)
