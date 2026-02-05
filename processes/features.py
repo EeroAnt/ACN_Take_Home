09070419b00bf2d4d40a46152137abe09f290498
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-05 19:17:20 +0200

    revised features

--- processes/features.py
+++ processes/features.py
@@ -2,17 +2,19 @@ import pandas as pd
 import sqlite3
 
 def feature_report(conn: sqlite3.Connection) -> pd.DataFrame:
-  df = get_dataframe(conn)
+  df = get_feature_dataframe(conn)
   preview_features(df)
     
 
-def get_dataframe(conn: sqlite3.Connection) -> pd.DataFrame:
+def get_feature_dataframe(conn: sqlite3.Connection) -> pd.DataFrame:
   return pd.read_sql_query(
     """
       SELECT 
           customer_id,
           COUNT(*) as transaction_count,
-          AVG(amount) as avg_amount,
+          ROUND(AVG(amount), 2) as avg_amount,
+          ROUND(MAX(amount), 2) as max_amount,
+          ROUND(MIN(amount), 2) as min_amount,
           SUM(amount) as total_spent,
           MIN(timestamp) as first_transaction,
           MAX(timestamp) as last_transaction,
@@ -30,7 +32,7 @@ def preview_features(df: pd.DataFrame) -> None:
   show_top_ten(df, "avg_days_between_transactions", True)
   show_top_ten(df, "total_spent", False)
   show_top_ten(df, "transaction_count", False)
-  show_top_ten(df, "avg_amount", False)
+  show_top_ten(df, "max_amount", False)
 
 def show_top_ten(df: pd.DataFrame, column: str, ascending: bool) -> None:
   sorted_df = df.sort_values(column, ascending=ascending)
