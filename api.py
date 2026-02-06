e6c1dfda4035843a0c05b6385d585fa2ce97c2df
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-06 20:03:30 +0200

    Update the API. /ask endpoint takes in query, top-k and threshold and even uses two of them. added /upload endpoint to expand the context pool

--- api.py
+++ api.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
-from processes.rag import retrieve, generate_response
+from processes.rag import generate_response, retrieve, vectorize
+from utils.filename_validation import validate_filename
 
 app = FastAPI()
 
 @app.post("/ask")
-def ask(query: str):
-    context = retrieve()
-    response = generate_response(context)
-    return {"query": query, "response": response}
+def ask(query: str, top_k: int, threshold: float):
+  embedding = vectorize(query)
+  context = retrieve(embedding, top_k, threshold)
+  response = generate_response(context)
+  return {"query": query, "response": response}
+
+@app.post("/upload")
+def upload(filename: str, content: str):
+  validated_filename = validate_filename(filename)
+  if type(validated_filename) == str:
+    with open(f"./data/documents/{validated_filename}", "w") as file:
+      file.write(content)
+    return { "status": 200 }
+  else:
+    return { "status": 403, "details": validated_filename["reason"] }
