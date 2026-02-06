7728ef296fbabdb55a6f1e08c71cfbcfc4a9cfe6
Eero Antikainen <eeroantikainen90@gmail.com>
2026-02-06 19:31:13 +0200

    Added a func to get all filenames from the documents folder

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -1,3 +1,9 @@
+import os
+
 def read_doc(filepath: str) -> str:
   with open(filepath, "r") as file:
     return file.read()
+
+def get_filenames():
+    folder_path = "./data/documents/"
+    return [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
